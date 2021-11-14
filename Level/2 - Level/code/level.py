9bf50b48fa1f6adad313d39b72bdd46c68dc2481
Artjom Nikokošev <anikok@ttu.ee>
2021-11-14 22:06:34 +0200

    ?

---
+++ Level/2 - Level/code/level.py
@@ -0,0 +1,38 @@
+import pygame
+from support import import_csv_layout, import_cut_graphics
+from settings import tile_size, screen_height, screen_width
+from tiles import Tile, StaticTile, Coin, Palm
+
+
+class Level:
+    def __init__(self, level_data, surface):
+        # general setup
+        self.display_surface = surface
+        self.world_shift = -3
+
+        terrain_layout = import_csv_layout(level_data['terrain'])
+        self.terrain_sprites = self.create_tile_group(terrain_layout, 'terrain')
+
+    def create_tile_group(self, layout, type):
+        sprite_group = pygame.sprite.Group()
+
+        for row_index, row in enumerate(layout):
+            for col_index, val in enumerate(row):
+                print(val)
+                if val != '-1':
+                    x = col_index * tile_size
+                    y = row_index * tile_size
+
+                    if type == 'terrain':
+                        terrain_tile_list = import_cut_graphics('../graphics/terrain/terrain_tiles.png')
+                        tile_surface = terrain_tile_list[int(val)]
+                        sprite = StaticTile(tile_size, x, y, tile_surface)
+
+                    sprite_group.add(sprite)
+
+        return sprite_group
+
+    def run(self):
+        # terrain
+        self.terrain_sprites.update(self.world_shift)
+        self.terrain_sprites.draw(self.display_surface)
