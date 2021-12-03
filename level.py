b78d0c765cfcd7a5966b8a6791e612a82c9c9531
Artjom Nikokošev <anikok@ttu.ee>
2021-12-03 23:07:49 +0200

    new level design, running dust particles, particularly fixed bug with objects collision

--- level.py
+++ level.py
@@ -2,7 +2,7 @@ import pygame
 from blocks import *
 from settings import *
 from player import Player
-
+# from particles import ParticleEffect
 
 class Level:
     """Level init."""
@@ -12,6 +12,15 @@ class Level:
         self.setup_level(level_data)
         self.world_shift_horizontal = 0
         self.world_shift_vertical = 0
+        self.current_x = 0
+
+        # dust
+        self.dust_sprite = pygame.sprite.GroupSingle()
+
+    def create_jump_particles(self, pos):
+        jump_particle_sprite = ParticleEffect(pos, 'jump')
+        # self.dust_sprite.add(jump_particle_sprite)
+        pass
 
     def setup_level(self, layout):
         self.sprites_blocks = pygame.sprite.Group()
@@ -53,7 +62,7 @@ class Level:
                     self.sprites_blocks.add(door)
 
                 if block_type == 'P':
-                    player = Player((x, y), (block_size, block_size))
+                    player = Player((x, y), (block_size, block_size), self.display_surface, self.create_jump_particles)
                     self.sprites_player.add(player)
 
                 if block_type == 'E':
@@ -82,7 +91,7 @@ class Level:
     def collision_horizontal(self):
         """Handle player and objects collision with horizontal movement."""
         player = self.sprites_player.sprite
-        player.rect.x += player.direction.x
+        player.rect.x += player.direction.x * player.moving_speed
 
         for block in self.sprites_blocks.sprites():
             # If player collides with any block.
@@ -90,9 +99,19 @@ class Level:
                 # If player is moving left and colliding some block => collision happens on the left side of player.
                 if player.direction.x < 0:
                     player.rect.left = block.rect.right
+                    player.on_left = True
+                    self.current_x = player.rect.left
                 # If player is moving right and colliding some block => collision happens on the right side of player.
                 elif player.direction.x > 0:
                     player.rect.right = block.rect.left
+                    player.on_right = True
+                    self.current_x = player.rect.right
+
+        if player.on_left and (player.rect.left < self.current_x or player.direction.x >= 0):
+            player.on_left = False
+        if player.on_right and (player.rect.right > self.current_x or player.direction.x <= 0):
+            player.on_right = False
+
 
     def collision_vertical(self):
         """Handle player and objects collision with vertical movement."""
@@ -108,12 +127,19 @@ class Level:
                 if player.direction.y > 0:
                     player.rect.bottom = block.rect.top
                     player.direction.y = 0  # Set vertical movement to zero when player is standing on something.
+                    player.on_ground = True
                     # Player can jump only if it standing on something (avoid double-jump).
                     player.jump()
 
                 elif player.direction.y < 0:
                     player.rect.top = block.rect.bottom
                     player.direction.y = 0  # If player hits something with its head => cancel jumping (avoid sliding while jumping and hitting e.g. ceiling).
+                    player.on_ceiling = True
+
+        if player.on_ground and player.direction.y < 0 or player.direction.y > 1: # if player is jumping or falling, then he isn't on the floor.
+            player.on_ground = False
+        if player.on_ceiling and player.direction.y > 0:
+            player.on_ceiling = False
 
         player.message(str(player.rect.width))  # TEST, remove later
 
@@ -121,11 +147,20 @@ class Level:
         """Handle player and enemy collision."""
         pass
 
+    def check_win(self):
+        if pygame.sprite.spritecollide(self.sprites_player, self.sprites_blocks(img_door_metal_closed, False)):
+            self.setup_level(level_2)
+
+
     def run(self):
         """Run (i.e. draw i.e. render) the level."""
         self.sprites_blocks.update(self.world_shift_horizontal, self.world_shift_vertical)
         self.sprites_blocks.draw(self.display_surface)
         self.scroll_x()
+
+        # dust
+        # self.dust_sprite.update((self.world_shift_horizontal, self.world_shift_vertical))
+        # self.dust_sprite.draw(self.display_surface)
 
         self.sprites_player.update()
         self.collision_horizontal()
